673e053af10264c192ea06b6d3a0964baeb969bd
Plavskikh Peter <qksyo@yandex.ru>
2022-08-09 04:10:43 +0700

    feat: aboutMe component

---
+++ movies-explorer-frontend/src/components/Main/AboutMe/AboutMe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import photo from "../../../images/photo.jpg"
+
+export default function AboutMe() {
+    return (
+        <div className="aboutMe">
+            <h2 className="aboutMe__title">Студент</h2>
+            <div className="aboutMe__container">
+                <div className="aboutMe__info-box">
+                    <h3 className="aboutMe__name">Петр</h3>
+                    <p className="aboutMe__brief">Фронтенд-разработчик, 21 год</p>
+                    <p className="aboutMe__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
+и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке,
+ начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <div className="aboutMe__links">
+                        <a href="#" className="aboutMe__link">Facebook</a>
+                        <a href="#" className="aboutMe__link">Github</a>
+                    </div>
+                </div>
+                <img src={photo} alt="фотография" className="aboutMe__photo"></img>
+            </div>
+        </div>
+    );
+};
--- movies-explorer-frontend/src/components/Main/Main.js
+++ movies-explorer-frontend/src/components/Main/Main.js
@@ -4,6 +4,7 @@ import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
 import AboutProject from "./AboutProject/AboutProject";
 import Techs from "./Techs/Techs";
+import AboutMe from "./AboutMe/AboutMe";
 
 export function Main() {
     return (
@@ -13,6 +14,7 @@ export function Main() {
             <NavTab></NavTab>
             <AboutProject></AboutProject>
             <Techs></Techs>
+            <AboutMe></AboutMe>
         </>
     )
 }
